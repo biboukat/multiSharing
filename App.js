2c9a238686af6c2db9459170c44ab280f95a4eba
Alexander Kovtun <akovtun@gmail.com>
2019-04-15 18:03:01 +0300

    added facebook sdk

--- App.js
+++ App.js
@@ -6,24 +6,77 @@
  * @flow
  */
 
-import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
+import React, { Component } from "react";
+import {
+  Platform,
+  StyleSheet,
+  Text,
+  View,
+  TouchableHighlight
+} from "react-native";
+import { LoginButton, AccessToken, ShareDialog } from "react-native-fbsdk";
 
 const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
+  ios: "Press Cmd+R to reload,\n" + "Cmd+D or shake for dev menu",
   android:
-    'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
+    "Double tap R on your keyboard to reload,\n" +
+    "Shake or press menu button for dev menu"
 });
 
 type Props = {};
 export default class App extends Component<Props> {
+  state = {
+    shareLinkContent: {
+      contentType: "link",
+      contentUrl: "https://www.facebook.com/",
+      contentDescription: "Facebook sharing is easy!"
+    }
+  };
+  _shareLink = () => {
+    const tmp = this;
+    ShareDialog.canShow(this.state.shareLinkContent)
+      .then(function(canShow) {
+        if (canShow) {
+          return ShareDialog.show(tmp.state.shareLinkContent);
+        }
+      })
+      .then(
+        function(result) {
+          if (result.isCancelled) {
+            alert("Share cancelled");
+          } else {
+            alert("Share success with postId: " + result.postId);
+          }
+        },
+        function(error) {
+          alert("Share fail with error: " + error);
+        }
+      );
+  };
+
   render() {
     return (
       <View style={styles.container}>
+        <LoginButton
+          onLoginFinished={(error, result) => {
+            if (error) {
+              console.log("login has error: " + result.error);
+            } else if (result.isCancelled) {
+              console.log("login is cancelled.");
+            } else {
+              AccessToken.getCurrentAccessToken().then(data => {
+                console.log(data.accessToken.toString());
+              });
+            }
+          }}
+          onLogoutFinished={() => console.log("logout.")}
+        />
         <Text style={styles.welcome}>Welcome to React Native!</Text>
         <Text style={styles.instructions}>To get started, edit App.js</Text>
         <Text style={styles.instructions}>{instructions}</Text>
+        <TouchableHighlight onPress={this._shareLink}>
+          <Text>Share bla</Text>
+        </TouchableHighlight>
       </View>
     );
   }
@@ -32,18 +85,18 @@ export default class App extends Component<Props> {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#F5FCFF"
   },
   welcome: {
     fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
+    textAlign: "center",
+    margin: 10
   },
   instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+    textAlign: "center",
+    color: "#333333",
+    marginBottom: 5
+  }
 });
