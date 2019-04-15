cbc98de78cc48a547a96d0d13084a2650342fcd6
Alexander Kovtun <akovtun@gmail.com>
2019-04-15 18:31:40 +0300

    add image picker

--- App.js
+++ App.js
@@ -6,32 +6,42 @@
  * @flow
  */
 
-import React, { Component } from "react";
+import React, { Component } from 'react';
 import {
   Platform,
   StyleSheet,
   Text,
   View,
-  TouchableHighlight
-} from "react-native";
-import { LoginButton, AccessToken, ShareDialog } from "react-native-fbsdk";
+  TouchableHighlight,
+  Image,
+} from 'react-native';
+import { LoginButton, AccessToken, ShareDialog } from 'react-native-fbsdk';
+import ImagePicker from 'react-native-image-picker';
 
 const instructions = Platform.select({
-  ios: "Press Cmd+R to reload,\n" + "Cmd+D or shake for dev menu",
+  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
   android:
-    "Double tap R on your keyboard to reload,\n" +
-    "Shake or press menu button for dev menu"
+    'Double tap R on your keyboard to reload,\n' +
+    'Shake or press menu button for dev menu',
 });
 
-type Props = {};
-export default class App extends Component<Props> {
+const options = {
+  title: 'Select photo',
+  storageOptions: {
+    skipBackup: true,
+    path: 'images',
+  },
+};
+
+export default class App extends Component {
   state = {
     shareLinkContent: {
-      contentType: "link",
-      contentUrl: "https://www.facebook.com/",
-      contentDescription: "Facebook sharing is easy!"
-    }
+      contentType: 'link',
+      contentUrl: 'https://www.facebook.com/',
+      contentDescription: 'Facebook sharing is easy!',
+    },
   };
+
   _shareLink = () => {
     const tmp = this;
     ShareDialog.canShow(this.state.shareLinkContent)
@@ -43,15 +53,39 @@ export default class App extends Component<Props> {
       .then(
         function(result) {
           if (result.isCancelled) {
-            alert("Share cancelled");
+            alert('Share cancelled');
           } else {
-            alert("Share success with postId: " + result.postId);
+            alert('Share success with postId: ' + result.postId);
           }
         },
         function(error) {
-          alert("Share fail with error: " + error);
-        }
+          alert('Share fail with error: ' + error);
+        },
       );
+  };
+
+  _sharePhoto = () => {
+    ImagePicker.showImagePicker(options, response => {
+      console.log('Response = ', response);
+
+      if (response.didCancel) {
+        console.log('User cancelled image picker');
+      } else if (response.error) {
+        console.log('ImagePicker Error: ', response.error);
+      } else if (response.customButton) {
+        console.log('User tapped custom button: ', response.customButton);
+      } else {
+        const source = { uri: response.uri };
+        console.log('bla source', source);
+
+        // You can also display the image using data:
+        // const source = { uri: 'data:image/jpeg;base64,' + response.data };
+
+        // this.setState({
+        //   avatarSource: source
+        // });
+      }
+    });
   };
 
   render() {
@@ -60,16 +94,16 @@ export default class App extends Component<Props> {
         <LoginButton
           onLoginFinished={(error, result) => {
             if (error) {
-              console.log("login has error: " + result.error);
+              console.log('login has error: ' + result.error);
             } else if (result.isCancelled) {
-              console.log("login is cancelled.");
+              console.log('login is cancelled.');
             } else {
               AccessToken.getCurrentAccessToken().then(data => {
                 console.log(data.accessToken.toString());
               });
             }
           }}
-          onLogoutFinished={() => console.log("logout.")}
+          onLogoutFinished={() => console.log('logout.')}
         />
         <Text style={styles.welcome}>Welcome to React Native!</Text>
         <Text style={styles.instructions}>To get started, edit App.js</Text>
@@ -77,6 +111,10 @@ export default class App extends Component<Props> {
         <TouchableHighlight onPress={this._shareLink}>
           <Text>Share bla</Text>
         </TouchableHighlight>
+        <TouchableHighlight onPress={this._sharePhoto}>
+          <Text>Share some photo</Text>
+        </TouchableHighlight>
+        <Image />
       </View>
     );
   }
@@ -85,18 +123,18 @@ export default class App extends Component<Props> {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-    backgroundColor: "#F5FCFF"
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
   },
   welcome: {
     fontSize: 20,
-    textAlign: "center",
-    margin: 10
+    textAlign: 'center',
+    margin: 10,
   },
   instructions: {
-    textAlign: "center",
-    color: "#333333",
-    marginBottom: 5
-  }
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
 });
